32f50c4c5b87adef9d365fcb6577138b2faccf5e
imbolc <me@imbolc.name>
2022-07-04 17:04:11 +0300

    Clippy

--- src/error.rs
+++ src/error.rs
@@ -2,13 +2,15 @@
 
 /// A helper to format error with its source chain
 pub fn chain(e: &impl std::error::Error) -> String {
+    use std::fmt::Write as _;
+
     let mut s = e.to_string();
     let mut current = e.source();
     if current.is_some() {
         s.push_str("\nCaused by:");
     }
     while let Some(cause) = current {
-        s.push_str(&format!("\n\t{}", cause));
+        write!(s, "\n\t{}", cause).ok();
         current = cause.source();
     }
     s
